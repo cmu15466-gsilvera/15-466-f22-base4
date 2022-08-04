994f9435d81e372d617cd15add7361e34f2d0570
Gustavo Silvera <grs5382@gmail.com>
2022-08-03 21:13:55 -0700

    format with clang

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -5,44 +5,43 @@
 
 #include <glm/glm.hpp>
 
-#include <vector>
 #include <deque>
+#include <vector>
 
 struct PlayMode : Mode {
-	PlayMode();
-	virtual ~PlayMode();
+    PlayMode();
+    virtual ~PlayMode();
 
-	//functions called by main loop:
-	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
-	virtual void update(float elapsed) override;
-	virtual void draw(glm::uvec2 const &drawable_size) override;
+    // functions called by main loop:
+    virtual bool handle_event(SDL_Event const&, glm::uvec2 const& window_size) override;
+    virtual void update(float elapsed) override;
+    virtual void draw(glm::uvec2 const& drawable_size) override;
 
-	//----- game state -----
+    //----- game state -----
 
-	//input tracking:
-	struct Button {
-		uint8_t downs = 0;
-		uint8_t pressed = 0;
-	} left, right, select;
+    // input tracking:
+    struct Button {
+        uint8_t downs = 0;
+        uint8_t pressed = 0;
+    } left, right, select;
 
-	//local copy of the game scene (so code can change it during gameplay):
-	Scene scene;
+    // local copy of the game scene (so code can change it during gameplay):
+    Scene scene;
 
-	//hexapod leg to wobble:
-	Scene::Transform *hip = nullptr;
-	Scene::Transform *upper_leg = nullptr;
-	Scene::Transform *lower_leg = nullptr;
-	glm::quat hip_base_rotation;
-	glm::quat upper_leg_base_rotation;
-	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
+    // hexapod leg to wobble:
+    Scene::Transform* hip = nullptr;
+    Scene::Transform* upper_leg = nullptr;
+    Scene::Transform* lower_leg = nullptr;
+    glm::quat hip_base_rotation;
+    glm::quat upper_leg_base_rotation;
+    glm::quat lower_leg_base_rotation;
+    float wobble = 0.0f;
 
-	glm::vec3 get_leg_tip_position();
+    glm::vec3 get_leg_tip_position();
 
-	//music coming from the tip of the leg (as a demonstration):
-	std::shared_ptr< Sound::PlayingSample > leg_tip_loop;
-	
-	//camera:
-	Scene::Camera *camera = nullptr;
+    // music coming from the tip of the leg (as a demonstration):
+    std::shared_ptr<Sound::PlayingSample> leg_tip_loop;
 
+    // camera:
+    Scene::Camera* camera = nullptr;
 };
